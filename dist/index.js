d20989c63b87953d203c9b7a3a352be4f22ca560
Alucard163 <romemix163@gmail.com>
2023-04-26 17:55:41 +0400

    add express, ts

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+import { userRouter } from "./users/users.js";
+import express from 'express';
+const port = 8000;
+const app = express();
+app.get('/hello', (req, res) => {
+    res.send('Привет!');
+    res.end();
+});
+app.use('/users', userRouter);
+app.use((err, req, res, next) => {
+    console.log(err.message);
+    res.status(401).send(err.message);
+});
+app.listen(port, () => {
+    console.log(`Сервер запущен на http://localhost:${port}`);
+});
